b2af85e79dd522ea8f367a581875e8059ed45728
ISONE-DEVOPS <isone.devops@isone.cv>
2023-01-30 14:23:27 -0100

    Initial commit

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,36 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audioplayers/web/audioplayers_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_for_web/connectivity_for_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:location_web/location_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:vibration_web/vibration_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioplayersPlugin.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlugin.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VibrationWebPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
